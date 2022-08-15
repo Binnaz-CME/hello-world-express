75a484bc0ff728e7bd8a781cc48076d7c8cfee10
Binnaz Akyüz <binnaz.akyuz@cmeducations.se>
2022-08-15 10:28:59 +0200

    second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Binnaz!");
+  res.send("Hello Binnaz World!");
 });
 
 app.listen(PORT, () => {
